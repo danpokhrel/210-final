537f66c2630fb3392b8d660a380900fb5247c2aa
Dan <texhnistguru@gmail.com>
2024-12-09 18:43:32 -0800

    Commit 1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 // COMSC-210 | Final | Dan Pokhrel
 #include <iostream>
+#include <list>
+#include <string>
+#include <vector>
+#include <fstream>
 using namespace std;
 
+const int SIM_ROUNDS = 10;
+const int JOIN_PROB = 50;
+
+struct Customer{
+    string name;
+    string order;
+};
+
 int main(){
-    cout << "Hello World";
+    ifstream fin("names.txt");
+    vector<string> names;
+    while(fin.good()){
+        string buf;
+        
+    }
+    
+    list<Customer> coffee_booth;
 
     return 0;
 }
